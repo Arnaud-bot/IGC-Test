e76798753a5e47be304ab3e96105e77f6cc7d426
Arnaud Malanda <arnaudmalanda1@gmail.com>
2026-05-28 20:54:44 +0400

    Premier projet React avec Vite

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+function App() {
+  return (
+    <div>
+      <video src="./public/IGC.mp4"
+        type="video/mp4"
+        width="800"
+        height="450"
+        controls
+        autoPlay
+        muted
+        loop
+        poster="./public/IGC.mp4/ik-thumbnail.jpg?tr=so-18">
+
+        <track
+          kind="subtitles"
+          src="public/english.vtt"
+          srcLang="en"
+          label="English"
+          default>
+        </track>
+
+        <track
+          kind="subtitles"
+          src="/public/hindi.vtt"
+          srcLang="hi"
+          label="Hindi"
+          default>
+        </track>
+
+
+      </video>
+    </div>
+  )
+}
+
+export default App
